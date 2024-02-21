8a6fc26c55716e158f83d097e96045675dc8083b
wawrzu <kamwawrzak@gmail.com>
2024-02-21 20:20:29 +0100

    separate cli and server

---
+++ cmd/server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+		"flag"
+		"log"
+
+		"github.com/kamwawrzak/sslverifier/internal/server"
+		"github.com/kamwawrzak/sslverifier/internal/service"
+)
+
+var defaultPort = 8089
+var trustedRootCAsPath = "./trusted-certs.pem"
+
+func main() {
+	var port int
+	flag.IntVar(&port, "port", defaultPort, "http server port" )
+	flag.Parse()
+
+	dialer := service.NewTcpDialer("tcp")
+	verifier := service.NewCertificateVerifier(dialer, trustedRootCAsPath)
+
+	handler := server.NewVerifyHandler(verifier)
+	server := server.NewServer(port, handler)
+
+	log.Printf("Starting http server on port: %d ...", port)
+	err := server.Start()
+	if err != nil {
+		log.Println(err)
+	}
+}
